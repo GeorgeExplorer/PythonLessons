c67b65edaa42ff4a2f5a7307009617c23102d0f5
GeoMat <matoshvili777@gmail.com>
2022-05-09 21:16:30 +0300

    Added output example data for using locale package of Russia

--- git_learning/learn.py
+++ git_learning/learn.py
@@ -2,3 +2,4 @@ import locale #импортировать библиотеку по работе
 loc = locale.getlocale() #запомнить текущую локаль
 locale.setlocale(locale.LC_ALL, "Russian_Russia.1251") #изменить локаль
 locale.setlocale(locale.LC_ALL, loc) #вернуть локаль
+print("%.2f", any value)
