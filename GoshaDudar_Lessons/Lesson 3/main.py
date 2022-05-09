a8811f52ccb2397241fa33d15931ebdf9c532dd4
GeoMat <matoshvili777@gmail.com>
2022-05-09 19:20:45 +0300

    Changed from Multiplication result to more Arifmetic results Changed location of the Division's operations Added some new math functions (abs, round, pow) Added a new func for input data by users

--- GoshaDudar_Lessons/Lesson 3/main.py
+++ GoshaDudar_Lessons/Lesson 3/main.py
@@ -2,7 +2,7 @@ print("First", "line", sep=" ") #sep="value" — директива-раздел
                                 # -> в инструкции print(), напр. sep="|" вернет | между значениями (1|2|"hello" etc.)
 
 print("Second line", end="!\n") #end="value" — аргумент, разделяющий строки кода любым символом или -> см.след.строку
-                               # -> переносом на новую строку (end="\n")
+                                # -> переносом на новую строку (end="\n")
 print("Third line for example 'end' argument from Second line")
 
 print("Special symbol 'backslash' for any symbols like \\ or \'") #обратный слэш позволяет обозначить спец.символ -> см.след.строку
@@ -12,12 +12,20 @@ print("1\n2\n3") #"[value1]\n[value2]\n[value3]" — перенос строко
 
 print("One \t Two") #"[value1]\t[value2]" — создание табуляции через экранированный символ \t
 
-print("Multiplication result: ", 5*5) #умножение, если будет + или - значит сложение и вычитание, соответственно
+print("Arifmetic results: ", 5*5, "|", 125+125, "|", 250-100) #умножение, сложение и вычитание
 
 print("Exponentiation result: ", 2**4) #возведение в степень
+print("Exponentiation result another way: ", pow(2, 4)) #другой способ возведения в степень, через аргумент pow(value, value for expon.)
 
-print("Division with integer result: ", 6//4, ",", 6//2) #деление без плавающей точки(запятой) — оно же целочисленное
+print("Division with integer result: ", 6//4, "|", 6//2) #деление без плавающей точки(запятой) — оно же целочисленное
+print("Division with float result: ", 10/4, "|", 6/2) #деление с остатком
 
-print("Division with float result: ", 6/4, ",", 6/2) #деление с остатком
+print("Arifmetic results with round function: ", round(10/4), "|", round(6+4.5)) #мат.операции с округлением чисел -> см.след.строку
+                                                                                 # -> через функцию round(value1+11value2)
 
-print("Looking for min value: ", min(-1, 0, 1, 2, 3, 4)) #нахождение минимального значения из списка
+print("Finding the min value: ", min(-1, 0, 1, 2, 3, 4)) #нахождение минимального значения из списка
+print("Finding the max value: ", max(-1, 0, 1, 2, 3, 4)) #нахождение максимального значения из списка
+
+print("Finding the module of some value: ", abs(-55)) #нахождение модуля числа (у отрицательного - положительное)
+
+input("Continue this expression: 25+25=") #приглашение для ввода входных данных со стороны пользователя
--- git_learning/learn.py
+++ git_learning/learn.py
@@ -1,9 +1,4 @@
 import locale #импортировать библиотеку по работе с локалями
 loc = locale.getlocale() #запомнить текущую локаль
 locale.setlocale(locale.LC_ALL, "Russian_Russia.1251") #изменить локаль
-salary = 50000.0
-print("Ваша ЗП на сегодня ", locale.format_string("%.2f", salary, grouping=True), sep="")
-#locale.setlocale(locale.LC_ALL, loc) #вернуть локаль
-
-input("Введите число: ")
-print(n + 5)
+locale.setlocale(locale.LC_ALL, loc) #вернуть локаль
