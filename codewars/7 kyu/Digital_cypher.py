8137a08d0a499b9bca1fae0642537ea97d86afc6
GeoMat <matoshvili777@gmail.com>
2022-05-25 17:15:21 +0400

    Commented last pie of the code

--- codewars/7 kyu/Digital_cypher.py
+++ codewars/7 kyu/Digital_cypher.py
@@ -1,6 +1,15 @@
 message = ["a", "b", "c", "d", "e", "f"]
-index = [1, 2, 3, 4, 5, 6]
+output = []
 key = 100
+output_result = []
+pie_key = [int(item) for item in str(key)]
+print(pie_key)
 
-for letter in message:
-    letter
+for character in message:
+    number = ord(character) - 96
+    output.append(number)
+
+for value in output:
+    new_value = value + value in pie_key
+    output_result.append(new_value)
+# print(output_result)
