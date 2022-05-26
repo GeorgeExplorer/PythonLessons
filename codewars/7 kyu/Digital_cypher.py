e1df21557450ab717c7c1a857115d518556ad8a5
GeorgeExplorer <matoshvili777@gmail.com>
2022-05-27 02:30:06 +0400

    Resolved the task by 7kyu "Digital cypher"

--- codewars/7 kyu/Digital_cypher.py
+++ codewars/7 kyu/Digital_cypher.py
@@ -1,14 +1,23 @@
 message = ["a", "b", "c", "d", "e", "f"]
 output = []
-key = [100]
-pie_key = key[0].split()
+key = 240
+i = 0
+pie_key = [int(item) for item in str(key)]
 print(pie_key)
-# pie_key = [int(item) for item in str(key)]
-# print(pie_key)
 
 for character in message:
-    number = ord(character) - 96
+    number = ord(character) + pie_key[i] - 96
     output.append(number)
+    i = (i + 1) % len(pie_key)
+print(output)
+# output_result = []
+# print(output_result)
 
-output_result = list(map(lambda x, y: x + y, pie_key, output))
-print(output_result)
+
+# b = 0
+# n = 0
+# while a < len(output):
+#     output_result.append(pie_key+output)
+#     # b = b + 1
+#     # a = a + 1
+#     # n = b % 4
