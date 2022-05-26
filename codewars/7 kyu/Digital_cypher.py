13fb2f9c2a8c30401f721aae66a064843a84de8b
GeoMat <matoshvili777@gmail.com>
2022-05-26 16:33:51 +0400

    Added additional part of the code to sum elements of two arrays

--- codewars/7 kyu/Digital_cypher.py
+++ codewars/7 kyu/Digital_cypher.py
@@ -1,15 +1,14 @@
 message = ["a", "b", "c", "d", "e", "f"]
 output = []
-key = 100
-output_result = []
-pie_key = [int(item) for item in str(key)]
+key = [100]
+pie_key = key[0].split()
 print(pie_key)
+# pie_key = [int(item) for item in str(key)]
+# print(pie_key)
 
 for character in message:
     number = ord(character) - 96
     output.append(number)
 
-for value in output:
-    new_value = value + value in pie_key
-    output_result.append(new_value)
-# print(output_result)
+output_result = list(map(lambda x, y: x + y, pie_key, output))
+print(output_result)
