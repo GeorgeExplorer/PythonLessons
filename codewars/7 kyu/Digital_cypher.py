bb04a1dc07205234da6b28b3c07115f7a1ecaf89
GeorgeExplorer <99310998+GeorgeExplorer@users.noreply.github.com>
2022-05-29 06:30:21 +0400

    Update Digital_cypher.py

--- codewars/7 kyu/Digital_cypher.py
+++ codewars/7 kyu/Digital_cypher.py
@@ -10,14 +10,3 @@ for character in message:
     output.append(number)
     i = (i + 1) % len(pie_key)
 print(output)
-# output_result = []
-# print(output_result)
-
-
-# b = 0
-# n = 0
-# while a < len(output):
-#     output_result.append(pie_key+output)
-#     # b = b + 1
-#     # a = a + 1
-#     # n = b % 4
